65330771e70db4ae9d9b73b3f4b9cbcd4c303595
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-02-04 08:19:03 -0400

    chore: fix import

--- src/sisgea/(common)/(devops)/db-event/(model)/db-event-model/ISisgeaDbEventModel.ts
+++ src/sisgea/(common)/(devops)/db-event/(model)/db-event-model/ISisgeaDbEventModel.ts
@@ -1,4 +1,4 @@
-import { ISisgeaDbEventGenericModel } from '../(generic)';
+import { ISisgeaDbEventGenericModel } from '../ISisgeaDbEventGenericModel';
 import { ISisgeaDbEventModelUsuario } from '../db-event-model-usuario/ISisgeaDbEventModelUsuario';
 
 export type ISisgeaDbEventModel = ISisgeaDbEventModelUsuario | ISisgeaDbEventGenericModel;
