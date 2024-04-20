7401b1c403335e3f4647e98af389066c07253801
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-04-20 14:33:44 -0400

    fix(js/spec): GetDeclaration: type inference

--- javascript/packages/sisgea-spec/src/helpers/declareEntity.ts
+++ javascript/packages/sisgea-spec/src/helpers/declareEntity.ts
@@ -2,5 +2,4 @@ import { IDeclaredEntity, IEntityDeclarationRaw } from '@/helpers/types';
 
 export const DeclareEntity = <Factory extends IDeclaredEntity>(factory: Factory) => factory;
 
-export const GetDeclaration = <Declaration extends IEntityDeclarationRaw, Factory extends IDeclaredEntity<Declaration>>(factory: Factory) =>
-  factory();
+export const GetDeclaration = <Declaration extends IEntityDeclarationRaw>(factory: () => Declaration) => factory();
