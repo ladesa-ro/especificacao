0cccec5a97fa07c2d0adf1f6e30cd6e1659baec4
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-04-20 11:37:16 -0400

    chore(js/spec): fix import path

--- javascript/packages/sisgea-spec/src/sisgea/base/imagem-arquivo/operations/imagem-arquivo-find-one/IImagemArquivoFindOneResultDto.ts
+++ javascript/packages/sisgea-spec/src/sisgea/base/imagem-arquivo/operations/imagem-arquivo-find-one/IImagemArquivoFindOneResultDto.ts
@@ -1,8 +1,7 @@
 import * as Dto from '@/index';
-import { IArquivoFindOneResultDto } from '../../../arquivo/operations';
-import { IImagemArquivoModel } from '../../IImagemArquivoModel';
 
-export interface IImagemArquivoFindOneResultDto extends Pick<IImagemArquivoModel, 'id' | 'largura' | 'altura' | 'formato' | 'mimeType'> {
+export interface IImagemArquivoFindOneResultDto
+  extends Pick<Dto.IImagemArquivoModel, 'id' | 'largura' | 'altura' | 'formato' | 'mimeType'> {
   imagem: Dto.IObjectUuid;
-  arquivo: IArquivoFindOneResultDto;
+  arquivo: Dto.IArquivoFindOneByIdResultDto;
 }
