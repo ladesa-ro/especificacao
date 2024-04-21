093ffd3724c31e99b6c372bfdab7e02b0c269ba0
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-04-21 13:11:31 -0400

    feat(js/spec): estado find all declaration

--- javascript/packages/sisgea-spec/src/core/IPaginatedResultDto.ts
+++ javascript/packages/sisgea-spec/src/core/IPaginatedResultDto.ts
@@ -1,23 +1,111 @@
-export type IPaginatedResultDtoMeta = {
-  itemsPerPage: number;
-  totalItems: number;
-  currentPage: number;
-  totalPages: number;
-  search: string;
-  sortBy: [string, 'DESC' | 'ASC'][];
-  filter?: Record<string, string | string[]>;
-};
+import * as Spec from '@/index';
 
-export type IPaginatedResultDtoLinks = {
-  first?: string;
-  previous?: string;
-  current: string;
-  next?: string;
-  last?: string;
-};
+export type IPaginatedResultDtoMeta = Spec.InferFactoryEntityType<typeof PaginatedResultDtoMetaDeclaration>;
+
+export const PaginatedResultDtoMetaDeclaration = Spec.DeclareEntity(() => {
+  return {
+    name: 'PaginatedResultDtoMeta',
+
+    properties: {
+      itemsPerPage: {
+        type: Spec.PropertyTypes.INTEGER,
+        description: '',
+        nullable: false,
+      },
+      totalItems: {
+        type: Spec.PropertyTypes.INTEGER,
+        description: '',
+        nullable: false,
+      },
+      currentPage: {
+        type: Spec.PropertyTypes.INTEGER,
+        description: '',
+        nullable: false,
+      },
+      totalPages: {
+        type: Spec.PropertyTypes.INTEGER,
+        description: '',
+        nullable: false,
+      },
+
+      search: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: false,
+      },
+
+      // sortBy: [string, 'DESC' | 'ASC'][];
+      // filter?: Record<string, string | string[]>;
+    },
+  };
+});
+
+export type IPaginatedResultDtoLinks = Spec.InferFactoryEntityType<typeof PaginatedResultDtoLinksDeclaration>;
+
+export const PaginatedResultDtoLinksDeclaration = Spec.DeclareEntity(() => {
+  return {
+    name: 'PaginatedResultDtoLinks',
+
+    properties: {
+      first: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: true,
+      },
+      previous: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: true,
+      },
+      current: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: true,
+      },
+      next: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: true,
+      },
+      last: {
+        type: Spec.PropertyTypes.STRING,
+        description: '',
+        nullable: true,
+      },
+    },
+  };
+});
 
 export type IPaginatedResultDto<T> = {
   data: T[];
   meta: IPaginatedResultDtoMeta;
-  links: IPaginatedResultDtoLinks;
+  links: Spec.IPaginatedResultDtoLinks;
 };
+
+export const PaginatedResultDtoDeclarationFactoryBuilder = (type: Spec.IDeclaredEntity<any>) =>
+  Spec.DeclareEntity(() => {
+    return {
+      name: 'EstadoFindAllResult',
+
+      properties: {
+        meta: {
+          nullable: false,
+          description: '',
+          type: Spec.PaginatedResultDtoMetaDeclaration,
+        },
+
+        data: {
+          type: type,
+          arrayOf: true,
+          nullable: false,
+          description: 'Resultados da busca atual.',
+        },
+
+        links: {
+          description: '',
+          nullable: false,
+          type: Spec.PaginatedResultDtoLinksDeclaration,
+        },
+      },
+    };
+  });
--- javascript/packages/sisgea-spec/src/sisgea/ambientes/estado/estado-find-all.operation.ts
+++ javascript/packages/sisgea-spec/src/sisgea/ambientes/estado/estado-find-all.operation.ts
@@ -1,3 +1,12 @@
 import * as Spec from '@/index';
 
-export interface IEstadoFindAllResultDto extends Spec.IPaginatedResultDto<Spec.IEstadoFindOneByIdResultDto> {}
+// ======================================
+
+export type IEstadoFindAllResultDto = Spec.InferFactoryEntityType<
+  typeof EstadoFindAllResultDeclaration,
+  Spec.IOutputDeclarationModes['OUTPUT']
+>;
+
+export const EstadoFindAllResultDeclaration = Spec.PaginatedResultDtoDeclarationFactoryBuilder(Spec.EstadoFindOneByIdResultDeclaration);
+
+// ======================================
