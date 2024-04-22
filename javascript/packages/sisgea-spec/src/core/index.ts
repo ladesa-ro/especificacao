533e7e3abc9a1c222c7e993fc6110da7f8a80912
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-04-22 14:41:32 -0400

    refactor(js/spec): rename object-id.declaration file

--- javascript/packages/sisgea-spec/src/core/index.ts
+++ javascript/packages/sisgea-spec/src/core/index.ts
@@ -1,6 +1,6 @@
 export * from './IPaginatedOptionsInputDto';
 export * from './ISearchInputDto';
 export * from './dated-object.declaration';
-export * from './object-id-declaration';
+export * from './object-id.declaration';
 export * from './object-uuid.declaration';
 export * from './paginated-result.declaration';
