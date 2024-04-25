c2cb38e980df69410c7dd2200db63ff648dcc4a7
Uriel Procópio <procopiouriel928@gmail.com>
2024-04-25 17:48:46 -0400

    refactor(js/spec): diario-professor  declaration

--- javascript/sisgea-spec/src/sisgea/ensino/diario-professor/operations/diario-professor-find-all/IDiarioProfessorFindAllResultDto.ts
+++
@@ -1,4 +0,0 @@
-import { IPaginatedResultDto } from '@/core';
-import { IDiarioProfessorFindOneResultDto } from '../diario-professor-find-one';
-
-export interface IDiarioProfessorFindAllResultDto extends IPaginatedResultDto<IDiarioProfessorFindOneResultDto> {}
