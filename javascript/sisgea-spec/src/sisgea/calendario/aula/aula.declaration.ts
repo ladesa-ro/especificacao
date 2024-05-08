b6d651872680b187c33c0782adcb08041b475ac1
Thiciane Frata <thicianefernanda892@gmail.com>
2024-05-07 22:48:11 -0300

    feat(js/spec): aula declarators

---
+++ javascript/sisgea-spec/src/sisgea/calendario/aula/aula.declaration.ts
@@ -0,0 +1,21 @@
+import { IEntityDate } from "@/core";
+import { IAmbienteModel } from "@/sisgea/ambientes";
+import { IDiarioModel } from "@/sisgea/ensino";
+import { IIntervaloDeTempoModel } from "../intervalo-de-tempo/intervalo-de-tempo.declaration";
+
+export interface IAulaModel {
+    //
+    id: string;
+    //
+    formato: null | string;
+    data: string;
+    //
+    intervaloDeTempo: null | IIntervaloDeTempoModel;
+    diario: IDiarioModel;
+    ambientePadrao: null | IAmbienteModel;
+    //
+    dateCreated: IEntityDate;
+    dateUpdated: IEntityDate;
+    dateDeleted: null | IEntityDate;
+    //
+  }
