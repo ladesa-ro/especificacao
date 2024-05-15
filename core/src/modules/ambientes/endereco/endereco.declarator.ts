9a9c5ab2add192610ba8b0ac3784dff1eededf03
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-15 15:18:18 -0400

    change(endereco): UniDeclarator

--- core/src/modules/ambientes/endereco/endereco.declarator.ts
+++ core/src/modules/ambientes/endereco/endereco.declarator.ts
@@ -1,71 +1,142 @@
-import { UniTypeEntity, UniTypeInteger, UniTypeReference, UniTypeString } from '../../../common/unispec/types';
-import { createDeclarator } from '../../../types';
-import { Cidade } from '../cidade';
+import {
+  UniDeclarator,
+  UniTypeEntity,
+  UniTypeInteger,
+  UniTypePick,
+  UniTypeReference,
+  UniTypeString,
+  UniTypeView,
+} from '../../../common/unispec/types';
+import { Tokens } from '../../tokens';
 
-export const Endereco = createDeclarator(() => ({
-  name: 'Endereco',
+const EnderecoEntity = UniTypeEntity({
+  id: 'uuid',
+  dated: true,
 
-  shape: UniTypeEntity({
-    id: 'uuid',
-    dated: true,
+  description: 'Endereco',
 
-    properties: {
-      cep: UniTypeString({ description: 'CEP', constraints: { ['x-cep']: true } }),
+  properties: {
+    cep: UniTypeString({
+      escription: 'CEP',
+      constraints: { ['x-cep']: true },
+    }),
 
-      logradouro: UniTypeString({ description: 'Logradouro' }),
+    logradouro: UniTypeString({
+      description: 'Logradouro',
+    }),
 
-      numero: UniTypeInteger({
-        description: 'Número',
-        constraints: { min: 0, integer: true, positive: true },
-      }),
+    numero: UniTypeInteger({
+      description: 'Número',
+      constraints: { min: 0, integer: true, positive: true },
+    }),
 
-      bairro: UniTypeString({ description: 'Bairro' }),
+    bairro: UniTypeString({
+      description: 'Bairro',
+    }),
 
-      complemento: UniTypeString({
-        default: null,
-        required: true,
-        nullable: true,
-        description: 'Complemento',
-      }),
+    complemento: UniTypeString({
+      default: null,
+      required: true,
+      nullable: true,
+      description: 'Complemento',
+    }),
 
-      pontoReferencia: UniTypeString({
-        default: null,
-        required: true,
-        nullable: true,
-        description: 'Ponto de referência',
-      }),
+    pontoReferencia: UniTypeString({
+      default: null,
+      required: true,
+      nullable: true,
+      description: 'Ponto de referência',
+    }),
 
-      cidade: UniTypeReference({ targetsTo: Cidade.name, description: 'Cidade' }),
-    },
-  }),
-
-  views: {
-    default: [
-      'id',
-      'dateCreated',
-      'dateUpdated',
-      'dateDeleted',
-      //
-      'cep',
-      'logradouro',
-      'numero',
-      'bairro',
-      'complemento',
-      'pontoReferencia',
-      'cidade@default',
-    ],
-    input: ['cep', 'logradouro', 'numero', 'bairro', 'complemento', 'pontoReferencia', 'cidade@findById'],
+    cidade: UniTypeReference({
+      targetsTo: Tokens.Cidade.Entity,
+      description: 'Cidade',
+    }),
   },
+});
+
+export const EnderecoView = UniTypeView({
+  name: Tokens.Endereco.Entity,
+  description: 'Visão completa de um Endereco.',
+  properties: EnderecoEntity.properties,
+});
+
+export const EnderecoFindOneInputView = UniTypeView({
+  name: Tokens.Endereco.Views.FindOneInput,
+  description: 'Dados de entrada para encontrar um Endereco por ID.',
+  properties: { ...UniTypePick(EnderecoEntity, { id: true }) },
+});
+
+export const EnderecoFindOneResultView = UniTypeView({
+  name: Tokens.Endereco.Views.FindOneResult,
+
+  partialOf: Tokens.Endereco.Entity,
+  description: 'Visão FindOne de um Endereco.',
+
+  properties: {
+    ...UniTypePick(EnderecoEntity, {
+      id: true,
+      //
+      cep: true,
+      logradouro: true,
+      numero: true,
+      bairro: true,
+      complemento: true,
+      pontoReferencia: true,
+      //
+      cidade: true,
+      //
+      dateCreated: true,
+      dateUpdated: true,
+      dateDeleted: true,
+    }),
+  },
+});
+
+export const EnderecoInputView = UniTypeView({
+  name: Tokens.Endereco.Views.Input,
+  description: 'Dados de entrada para um Endereco.',
+  properties: {
+    ...UniTypePick(EnderecoView, {
+      cep: true,
+      logradouro: true,
+      numero: true,
+      bairro: true,
+      complemento: true,
+      pontoReferencia: true,
+    }),
+
+    cidade: UniTypeReference({
+      targetsTo: Tokens.Cidade.Views.FindOneInput,
+      description: 'Cidade do Endereço.',
+    }),
+  },
+});
+
+// =======================================
+
+export const EnderecoDeclarator = UniDeclarator({
+  entity: Tokens.Endereco.Entity,
 
   operations: {
     crud: {
+      findById: {
+        input: Tokens.Endereco.Views.FindOneInput,
+        output: Tokens.Endereco.Views.FindOneResult,
+      },
+
+      deleteById: Tokens.Endereco.Views.FindOneInput,
+
+      create: Tokens.Endereco.Views.Input,
+      updateById: Tokens.Endereco.Views.Input,
+
       list: {
+        view: Tokens.Endereco.Views.FindOneResult,
         filters: [
-          //
-          ['estado.id', ['$eq']],
+          ['cidade.id', ['$eq']],
+          ['cidade.estado.id', ['$eq']],
         ],
       },
-      findById: true,
     },
   },
-}));
+});
