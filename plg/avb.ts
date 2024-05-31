babd3dadfb59057519a4aea7b6db4fdc2aad007b
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-31 18:43:12 -0400

    feat(plg): use @unispec/driver-quicktype

--- plg/avb.ts
+++ plg/avb.ts
@@ -1,35 +1,24 @@
-import { AllNodesVisitor, JsonSchemaGenerator } from "@unispec/compiler";
-import { IsUniNodeView } from "@unispec/core";
-import * as fs from "node:fs";
-import { FetchingJSONSchemaStore, InputData, JSONSchemaInput, quicktype } from "quicktype-core";
+import { UnispecInput } from "@unispec/driver-quicktype";
+import * as jetpack from "fs-jetpack";
+import { quicktype } from "quicktype-core";
+import { Framework } from "quicktype-core/dist/language/CSharp/language";
 import { ModulesProvider } from "../core";
 
 async function main() {
-  const generator = new JsonSchemaGenerator();
-  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
-
-  const nodes: Iterable<any> = AllNodesVisitor.VisitAll(ModulesProvider);
-
-  for (const node of nodes) {
-    if (IsUniNodeView(node)) {
-      const jsonSchemaType = generator.Compile(node);
-      const jsonSchemaTypeAsString = JSON.stringify(jsonSchemaType, null, 2);
-
-      await schemaInput.addSource({ name: node.name, schema: jsonSchemaTypeAsString });
-    }
-  }
-
-  const inputData = new InputData();
-  inputData.addInput(schemaInput);
+  const inputData = await UnispecInput.GetInputDataFromEntryPoint(ModulesProvider);
 
   const output = await quicktype({
     inputData,
     lang: "csharp",
+
+    rendererOptions: {
+      framework: Framework.SystemTextJson,
+      namespace: "Sisgha.Dtos",
+    },
   });
 
-  fs.writeFileSync("Sisgha.Dtos.cs", output.lines.join("\n"));
-
-  // console.log(lines.join("\n"));
+  jetpack.dir("out");
+  jetpack.write("out/Sisgha.Dtos.cs", output.lines.join("\n"));
 }
 
 main();
