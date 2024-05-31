2f63438b136707f4bcd4da55171b4d2da9b31830
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-30 23:55:14 -0400

    feat(plg): codegen playgroudn

---
+++ plg/avb.ts
@@ -0,0 +1,35 @@
+import { AllNodesVisitor, JsonSchemaGenerator } from "@unispec/compiler";
+import { IsUniNodeView } from "@unispec/core";
+import * as fs from "node:fs";
+import { FetchingJSONSchemaStore, InputData, JSONSchemaInput, quicktype } from "quicktype-core";
+import { ModulesProvider } from "../core";
+
+async function main() {
+  const generator = new JsonSchemaGenerator();
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+
+  const nodes: Iterable<any> = AllNodesVisitor.VisitAll(ModulesProvider);
+
+  for (const node of nodes) {
+    if (IsUniNodeView(node)) {
+      const jsonSchemaType = generator.Compile(node);
+      const jsonSchemaTypeAsString = JSON.stringify(jsonSchemaType, null, 2);
+
+      await schemaInput.addSource({ name: node.name, schema: jsonSchemaTypeAsString });
+    }
+  }
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+
+  const output = await quicktype({
+    inputData,
+    lang: "csharp",
+    rendererOptions: {},
+  });
+
+  fs.writeFileSync("k.ts", output.lines.join("\n"));
+  // console.log(lines.join("\n"));
+}
+
+main();
