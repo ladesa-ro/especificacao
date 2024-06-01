2ae5943cf40f3a797b150a54a0ae76c7ccd8ccfa
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-05-31 20:46:32 -0400

    feat(integrations/-codegen): init

--- integrations/-codegen/src/main.ts
+++ integrations/-codegen/src/main.ts
@@ -1,10 +1,14 @@
 import { UnispecInput } from "@unispec/driver-quicktype";
 import * as jetpack from "fs-jetpack";
+import * as path from "node:path";
 import { quicktype } from "quicktype-core";
 import { Framework } from "quicktype-core/dist/language/CSharp/language";
-import { ModulesProvider } from "../core";
+import { ModulesProvider } from "../../../core/dist";
 
-async function main() {
+const PATH_INTEGRATIONS = path.join(__dirname, "../../../integrations");
+const PATH_INTEGRATIONS_DOTNET = path.join(PATH_INTEGRATIONS, "dotnet");
+
+async function genCSharp() {
   const inputData = await UnispecInput.GetInputDataFromEntryPoint(ModulesProvider);
 
   const output = await quicktype({
@@ -13,12 +17,17 @@ async function main() {
 
     rendererOptions: {
       framework: Framework.SystemTextJson,
-      namespace: "Sisgha.Dtos",
+      namespace: "Sisgea.Spec.Dtos",
     },
   });
 
   jetpack.dir("out");
-  jetpack.write("out/Sisgha.Dtos.cs", output.lines.join("\n"));
+  jetpack.write(`${PATH_INTEGRATIONS_DOTNET}/Sisgea.Spec.Dtos/Sisgea.Spec.Dtos.cs`, output.lines.join("\n"));
+}
+
+async function main() {
+  await genCSharp();
+  process.exit(0);
 }
 
 main();
