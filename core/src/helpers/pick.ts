f636667d5d898e66b30e23dc8bb92ae9ffcfdd63
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-06-03 13:24:30 -0400

    chore(core): move src/modules/* to src/*

--- core/src/helpers/pick.ts
+++
@@ -1,3 +0,0 @@
-export const pick = <T extends Record<K, any>, K extends keyof T>(obj: T, propertiesToPick: K[]) => {
-  return Object.fromEntries(Object.entries(obj).filter(([key]) => propertiesToPick.includes(key as K))) as Pick<T, K>;
-};
