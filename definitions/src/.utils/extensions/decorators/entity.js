dde7fa326d5856b1432a4164cb14024b7ad1bb3e
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-08-27 10:22:34 -0400

    feat(definitions/utils/extensions): @entityPartialOf

---
+++ definitions/src/.utils/extensions/decorators/entity.js
@@ -0,0 +1,13 @@
+import { setTypeSpecNamespace } from "@typespec/compiler";
+import { $extension } from "@typespec/json-schema";
+import { METADATA_ENTITY_TOKEN, NAMESPACE_EXTENSIONS } from "../tokens.js";
+
+//
+
+export function $entity(context, target, entityToken) {
+  $extension(context, target, METADATA_ENTITY_TOKEN, entityToken.value);
+}
+
+setTypeSpecNamespace(NAMESPACE_EXTENSIONS, $entity);
+
+//
